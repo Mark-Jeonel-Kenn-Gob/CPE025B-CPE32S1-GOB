b819f7a7986a2c028dfec285d4dd39dee090e801
Mark-Jeonel-Kenn-Gob <qmjkegob@tip.edu.ph>
2026-02-27 16:11:38 +0800

    seatwork 67 js

---
+++ JavaScript_Essentials/sw7-1/script3.js
@@ -0,0 +1,13 @@
+function max(array) { 
+    let maxValue = array[0]; // should be zero po 
+    
+    for(let i = 1; i < array.length; i++) { 
+        if(array[i] > maxValue) { 
+            maxValue = array[i]; 
+        } 
+    } 
+    return maxValue; 
+} 
+ 
+console.log(max([1, 4, 6, 2]));  // -> 6 
+console.log(max([10, 4, 6, 2])); // -> 10 fixed yah
