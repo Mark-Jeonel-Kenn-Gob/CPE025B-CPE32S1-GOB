e9cf4dc9de245755a65d529981f5dea5b13503ae
Mark-Jeonel-Kenn-Gob <qmjkegob@tip.edu.ph>
2026-03-13 15:16:59 +0800

    midterm skills exam push 1

---
+++ midterm-skills-exam/task1.js
@@ -0,0 +1,9 @@
+function sumDeepStrictNumbers(arr) {
+    // Code Here
+}
+
+
+
+// Test Code
+const testArray1 = [10, ['5', [true, 5]], null, [undefined, [10, NaN]]];
+console.log(sumDeepStrictNumbers(testArray1));
---
+++ midterm-skills-exam/task6.js
@@ -0,0 +1,7 @@
+function firstUniqueChar(str) {
+    // Code Here
+}
+
+// Test Code
+console.log(firstUniqueChar('sTreSS'));
+console.log(firstUniqueChar('aabbc')); 
